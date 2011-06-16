550d574ba56872cf2bfe7046f97a866f11a6c398
Matthew Ogle <mogle@mac.com>
2011-06-16 21:00:32 +0200

    AUTOPLAY AT LAST

--- twitspace.js
+++ twitspace.js
@@ -1,4 +1,5 @@
 var twitspaceOn;
+var songmp3;
 
 (function() {
     
@@ -23,14 +24,45 @@ var twitspaceOn;
           },
           function(data) {
             var artist = data.response.artists[0].name;
+            var id = data.response.artists[0].id;
             console.log(artist);
       
             if (artist) {
               var image = "http://ws.audioscrobbler.com/2.0/?method=artist.getimageredirect&artist=" + encodeURI(artist) + "&api_key=b25b959554ed76058ac220b7b2e0a026&size=mega";
               console.log(image);
               $('body').css("background", "url(" + image + ") center center repeat").addClass('myspacerized');
+              playRandomSong(id);
             }
-          });
+      });
     }
-  });
+});
 }());
+
+function playRandomSong(id) {
+    $.getJSON("http://developer.echonest.com/api/v4/song/search?callback=test&bucket=tracks&bucket=id:7digital",
+        {
+          api_key: "N6E4NIOVYMTHNDM8J",
+          artist_id: id,
+          results: 3,
+          sort: "song_hotttnesss-desc",
+          format: "json"
+        },
+        function(data) {
+            songmp3 = false;
+            if (data.response.songs) {
+                theSong = data.response.songs[Math.floor(Math.random() * data.response.songs.length)];
+                if (theSong.tracks) {
+                    songmp3 = theSong.tracks[0].preview_url;
+                }
+            }   
+            if (songmp3) {
+                audio = document.getElementById('audio');
+                if (!audio) {
+                    $('body').append('<audio id="audio" src="" autoplay="true" loop="true">');
+                    audio = document.getElementById('audio');
+                }
+                audio.src = songmp3;
+            }
+        }
+    );
+}
