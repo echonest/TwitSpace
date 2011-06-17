168f9ad85c5e0929d7c65510bb638d158d3fe7cd
Tim <timbormans@gmail.com>
2011-06-17 02:19:48 +0200

    Fix background bug

--- twitspace.js
+++ twitspace.js
@@ -63,7 +63,7 @@ function ts_myspacifyTweet(tweet_txt) {
 
       var image = "http://ws.audioscrobbler.com/2.0/?method=artist.getimageredirect&artist=" + encodeURI(artist) + "&api_key=b25b959554ed76058ac220b7b2e0a026&size=mega";
       //console.log(image);
-      $('body').css("background", "url(" + image + ") center center repeat").addClass('myspacerized');
+      $('body').css("background-image", "url(" + image + ")").addClass('myspacerized');
       
       // play that funky music
       ts_playRandomSong(id);
